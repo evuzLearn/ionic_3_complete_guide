355a2e660bb6273baf9abc331b1e7652c173076e
evuz <jesus_jgb_45@hotmail.com>
2017-09-04 17:18:06 +0200

    Creating the required Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SettingsPage } from './../pages/settings/settings';
+import { QuotesPage } from './../pages/quotes/quotes';
+import { QuotePage } from './../pages/quote/quote';
+import { LibraryPage } from './../pages/library/library';
+import { FavoritesPage } from './../pages/favorites/favorites';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +29,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
