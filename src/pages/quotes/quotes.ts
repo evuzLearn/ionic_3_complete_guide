1c99aec486592bffd6730ab92f7cef6650be49f7
evuz <jesus_jgb_45@hotmail.com>
2017-09-05 10:04:55 +0200

    Adding Alerts to the Application

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 
 import { Quote } from './../../data/quote.interface';
 
@@ -10,15 +10,37 @@ import { Quote } from './../../data/quote.interface';
 export class QuotesPage implements OnInit {
   quoteGroup: { category: string, quotes: Quote[], icon: string };
 
-  constructor(private navParams: NavParams) { }
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
   }
 
-  onAddToFavorite(quote: Quote) {
-    console.log(quote.text);
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [{
+        text: 'Yes, go ahead',
+        handler: () => {
+          console.log('Ok');
+        }
+      }, {
+        text: 'No, I changed my mind!',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelled');
+        }
+      }]
+    })
+
+    alert.present();
   }
+
   // ionViewDidLoad() {
   //   this.quoteGroup = this.navParams.data;
   //   Add elvis operator(?) in template to use this approach
