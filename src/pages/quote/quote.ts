12a95b729b7c3a8fd75a9f21812e1cfd965358d3
evuz <jesus_jgb_45@hotmail.com>
2017-09-05 10:32:40 +0200

    Dismissing modals

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-quote',
@@ -7,11 +7,9 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class QuotePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(private viewCtrl: ViewController) { }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+  onClose() {
+    this.viewCtrl.dismiss();
   }
-
 }
