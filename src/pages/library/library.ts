1abf73c3f19f32d984c11e50ac762186872c0d2e
evuz <jesus_jgb_45@hotmail.com>
2017-09-05 09:13:08 +0200

    Using the quotes data

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+
+import { Quote } from './../../data/quote.interface';
+import quotes from '../../data/quotes';
 
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit{
+  quoteCollection: { category: string, quotes: Quote[], icon: string }[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ngOnInit() {
+    this.quoteCollection = quotes;
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
-
 }
