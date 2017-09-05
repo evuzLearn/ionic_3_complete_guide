4537ead6057abf4be5d0dfac7209de046d1fdb4d
evuz <jesus_jgb_45@hotmail.com>
2017-09-05 10:36:52 +0200

    Passing Data to a Modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { ViewController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+  author: string;
+  text: string;
 
-  constructor(private viewCtrl: ViewController) { }
+  constructor(
+    private viewCtrl: ViewController,
+    private navParams: NavParams
+  ) { }
+
+  ionViewDidLoad() {
+    this.author = this.navParams.get('person');
+    this.text = this.navParams.get('text');
+  }
 
   onClose() {
     this.viewCtrl.dismiss();
